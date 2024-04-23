305597359601a334375932850eba5439e9c99853
nanpython <ananddotb@gmail.com>
2024-04-23 12:27:05 +0530

    completed UI, db created, loaded sql-alchemy, req.txt added

--- quiz/__init__.py
+++ quiz/__init__.py
@@ -1,7 +1,20 @@
 from flask import Flask
 from quiz import pages
+
+from flask_sqlalchemy import SQLAlchemy
+import os
+
 def create_app():
     app = Flask(__name__)
+
+    # Get the base directory
+    basedir = os.path.abspath(os.path.dirname(__file__))
+
+    # Set the database URI
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'nanquiz.sqlite3')
+
+    # Create the SQLAlchemy database object
+    db = SQLAlchemy(app)
 
     app.register_blueprint(pages.bp)
     return app
--- quiz/pages.py
+++ quiz/pages.py
@@ -3,8 +3,32 @@ from flask import Blueprint, render_template
 bp = Blueprint("pages", __name__)
 
 @bp.route("/")
+def login():
+    return render_template("pages/login.html")
+
+@bp.route("/logout")
+def logout():
+    return login()
+
+@bp.route("/register")
+def register():
+    return render_template("pages/register.html")
+
+@bp.route("/home")
 def home():
     return render_template("pages/home.html")
+
+@bp.route("/qbank")
+def qbank():
+    return render_template("pages/qbank.html")
+
+@bp.route("/test_paper")
+def test_paper():
+    return render_template("pages/test_paper.html")
+
+@bp.route("/quiz")
+def quiz():
+    return render_template("pages/quiz.html")
 
 @bp.route("/about")
 def about():
